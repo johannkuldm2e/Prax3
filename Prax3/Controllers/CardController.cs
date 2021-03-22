e6c8c88922da9ee6d845df2f70e81e252dc72969
johannkuldm2e <38594359+johannkuldm2e@users.noreply.github.com>
2021-03-22 11:31:12 +0200

    Add project files.

---
+++ Prax3/Controllers/CardController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Prax3.Model;
+
+namespace Prax3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardController : ControllerBase
+    {
+        // GET: CardController
+        [HttpGet]
+        public Card GetCard([FromQuery] int cardNumber, int API_TOKEN)
+        {
+            if (cardNumber == 1234 && API_TOKEN == 12345)
+            {
+                return new Card("VALID", "VISA");
+            }
+            else
+            {
+                return new Card("INVALID", "NONE");
+            }
+
+        }
+    }
+}
---
+++ Prax3/Model/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Prax3.Model
+{
+    public class Card
+    {
+        public Card(string cardStatus, string cardType)
+        {
+        }
+        public string CardType { get; set; }
+
+
+        public string CardStatus { get; set; }
+
+        public int CardNumber = 1234;
+    }
+}
+
